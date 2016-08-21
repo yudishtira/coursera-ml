44c39ccd7e962fa2d59840c5a921935a3a2f174e
Sridhar Rao <sprao@fb.com>
2016-08-21 00:25:38 -0400

    nn complete

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -63,22 +63,41 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
+% Add bias unit to inputs
+a1 = [ones(size(X,1), 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
 
+% Add bias unit
+a2 = [ones(size(a2,1), 1) a2];
+z3 = a2 * Theta2';
+h = sigmoid(z3);
 
+y_matrix = eye(num_labels)(y,:);
 
+% Cost with feed forward values
+J = (-1/m) * sum(sum((y_matrix .* log(h)) + (1-y_matrix) .* log(1-h),2));
 
+% Adding regularization
+J =J + (lambda/(2*m)) * (sum(sum(Theta1(:,2:end).^2,2)) + sum(sum(Theta2(:,2:end).^2,2)));
 
+% Backpropagation
+d3 = h - y_matrix;
+d2 = sigmoidGradient(z2) .* (d3 * Theta2(:,2:end));
 
+delta1 = d2' * a1;
+delta2 = d3' * a2;
 
+% Regularization of Thetas
+% Set first colum to zeros
+Theta1(:,1) = 0;
+Theta2(:,1) = 0;
 
+Theta1 = (lambda/m).* Theta1;
+Theta2 = (lambda/m) * Theta2;
 
-
-
-
-
-
-
-
+Theta1_grad = ((1/m) * delta1) + Theta1;
+Theta2_grad = ((1/m) * delta2) + Theta2;
 
 % -------------------------------------------------------------
 
